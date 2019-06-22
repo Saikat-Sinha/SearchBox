aef06fbb78ffddc21d1cd04c36d8836417b2728a
Saikat-Sinha <saikat@myself.com>
2019-06-22 18:58:50 +0530

    Updates

---
+++ precache-manifest.1fbc307e4b89ec7783b5000f04405180.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1ef5ed92500795bd8faa31fddc8ef54",
+    "url": "/SearchBox/index.html"
+  },
+  {
+    "revision": "4856815975d0b8de0102",
+    "url": "/SearchBox/static/css/main.31f94c51.chunk.css"
+  },
+  {
+    "revision": "49f7dd68266b2b293ba9",
+    "url": "/SearchBox/static/js/2.f75dfc3a.chunk.js"
+  },
+  {
+    "revision": "4856815975d0b8de0102",
+    "url": "/SearchBox/static/js/main.0537db4a.chunk.js"
+  },
+  {
+    "revision": "3029d841bc721ecbec98",
+    "url": "/SearchBox/static/js/runtime~main.5b6278f8.js"
+  }
+]);
